d6483e11c347a241b735cb674114be05e7f2754c
Mark Longair <mhl@pobox.com>
2016-08-23 08:05:00 +0100

    Add minimal tests of the PopItImporter class

---
+++ popolo/tests/test_importer.py
@@ -0,0 +1,74 @@
+import json
+
+from django.test import TestCase
+
+from popolo.importers.popit import PopItImporter
+from popolo.models import Membership, Organization, Person
+
+class BasicImporterTests(TestCase):
+
+    def test_simplest_person(self):
+        input_json = '''
+{
+    "persons": [
+        {
+            "id": "a1b2",
+            "name": "Alice"
+        }
+
+    ],
+    "organizations": [],
+    "memberships": []
+}
+'''
+        data = json.loads(input_json)
+        importer = PopItImporter()
+        importer.import_from_export_json_data(data)
+        self.assertEqual(Person.objects.count(), 1)
+        person = Person.objects.get()
+        self.assertEqual(person.name, "Alice")
+        self.assertEqual(
+            person.identifiers.get(scheme='popit-person').identifier,
+            "a1b2")
+
+    def test_person_with_membership(self):
+        input_json = '''
+{
+    "persons": [
+        {
+            "id": "a1b2",
+            "name": "Alice"
+        }
+
+    ],
+    "organizations": [
+        {
+            "id": "commons",
+            "name": "House of Commons"
+        }
+    ],
+    "memberships": [
+        {
+            "person_id": "a1b2",
+            "organization_id": "commons"
+        }
+    ]
+}
+'''
+        data = json.loads(input_json)
+        importer = PopItImporter()
+        importer.import_from_export_json_data(data)
+        self.assertEqual(Membership.objects.count(), 1)
+        self.assertEqual(Person.objects.count(), 1)
+        self.assertEqual(Organization.objects.count(), 1)
+        person = Person.objects.get()
+        self.assertEqual(person.name, "Alice")
+        organization = Organization.objects.get()
+        self.assertEqual(organization.name, "House of Commons")
+        self.assertEqual(
+            organization.identifiers.get(scheme='popit-organization').identifier,
+            "commons"
+        )
+        membership = Membership.objects.get()
+        self.assertEqual(membership.person, person)
+        self.assertEqual(membership.organization, organization)
